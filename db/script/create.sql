783e8a86e39d01e6e304871a4161aea796a17f9c
xym <253274870@qq.com>
2018-07-12 18:20:56 +0800

    sql

--- db/script/create.sql
+++ db/script/create.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/7/10 14:18:20                           */
+/* Created on:     2018/7/11 17:39:01                           */
 /*==============================================================*/
 
 
@@ -9,12 +9,13 @@ drop table if exists PFM_SYS;
 /*==============================================================*/
 /* Table: PFM_SYS                                               */
 /*==============================================================*/
-create table PFM_SYS
+create  table PFM_SYS
 (
-   ID                   bigint not null comment '系统ID',
-   NAME                 varchar(100) not null comment '系统名字',
+   ID                   varchar(20) not null comment '系统ID',
+   NAME                 varchar(20) not null comment '系统名字',
    REMARK               varchar(100) not null comment '系统备注',
-   primary key (ID)
+   primary key (ID),
+   unique key AK_Key_2 (NAME)
 );
 
 alter table PFM_SYS comment '大卖系统管理';
