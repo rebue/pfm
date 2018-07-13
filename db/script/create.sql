abdb9447dac50f62a54c58ecd3fabde377d09c68
zbz <nnzbz@21cn.com>
2018-07-13 16:46:46 +0800

    * 修正powerdesign设计的部分细节错误

--- db/script/create.sql
+++ db/script/create.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/7/11 17:39:01                           */
+/* Created on:     2018/7/13 16:42:10                           */
 /*==============================================================*/
 
 
@@ -9,13 +9,14 @@ drop table if exists PFM_SYS;
 /*==============================================================*/
 /* Table: PFM_SYS                                               */
 /*==============================================================*/
-create  table PFM_SYS
+create table PFM_SYS
 (
    ID                   varchar(20) not null comment '系统ID',
-   NAME                 varchar(20) not null comment '系统名字',
+   NAME                 varchar(20) not null comment '系统名称',
    REMARK               varchar(100) not null comment '系统备注',
    primary key (ID),
-   unique key AK_Key_2 (NAME)
+   unique key AK_SYS_NAME (NAME)
 );
 
-alter table PFM_SYS comment '大卖系统管理';
+alter table PFM_SYS comment '系统信息';
+
